ff13619979993deb32b5244151218aa148151f02
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-05-07 23:24:02 +0300

    postmortem

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,11 +4,10 @@ Function that queries the Reddit API and prints
 the top ten hot posts of a subreddit
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
+    """Returns top ten posts"""
     u_agent = 'Mozilla/5.0'
 
     headers = {
@@ -20,17 +19,17 @@ def top_ten(subreddit):
     }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    res_json = response.json()
+    hot_posts = res_json['data']['children']
+    if response.status_code != 200:
+        print("None")
         return
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
+    if len(hot_posts) == 0:
+        print("None")
     else:
         for post in hot_posts:
             print(post['data']['title'])
