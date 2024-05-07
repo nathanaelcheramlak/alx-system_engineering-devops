06360797a422a074964e680cb4bef5ff684dace1
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-05-07 14:47:12 +0300

    minor update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,10 @@ Function that queries the Reddit API and returns
 the number of subscribers for a given subreddit.
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
+    """Returns the number of subscribers of a subreddit"""
     u_agent = 'Mozilla/5.0'
 
     headers = {
@@ -16,12 +15,10 @@ def number_of_subscribers(subreddit):
     }
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    response = requests.get(url, headers=headers,  allow_redirects=False)
+    res = response.json()
+    if response.status_code != 200:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+    if 'data' in res:
+        return res['data']['subscribers']
+    return 0
--- 0x16-api_advanced/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    print("LEn", len(response['data']))
-    print(response.status_code)
